ffb4f572778d3be743d08eafacaee945815f560c
Zhipeng-Chang <zchang0302@gmail.com>
2019-01-12 15:12:58 -0700

    works in chrome

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 #  coding: utf-8 
 import socketserver
-
+import os
 # Copyright 2013 Abram Hindle, Eddie Antonio Santos
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -32,6 +32,12 @@ import socketserver
 # Rference: https://stackoverflow.com/questions/29643544/python-a-bytes-like-object-is-required-not-str
 # answered by valentin Apr 15 '15 at 7:03
 
+# Reference: https://stackoverflow.com/questions/18563664/socketserver-python
+# answered by sberry Sep 1 '13 at 23:39
+
+# Reference: https://eclass.srv.ualberta.ca/pluginfile.php/4549769/mod_resource/content/2/04-HTTP.pdf
+# by Abram Hindle
+
 class MyWebServer(socketserver.BaseRequestHandler):
     
     def handle(self):
@@ -39,17 +45,51 @@ class MyWebServer(socketserver.BaseRequestHandler):
         print ("Got a request of: %s\n" % self.data)
         self.request.sendall(bytearray("OK",'utf-8'))
         try:
-            request_method = self.data.splitlines()[0].decode().split(" / ")[0]
+            request_method = self.data.splitlines()[0].decode().split(" ")[0]
             print ("Request method: %s\n" % request_method)
         except Exception as e:
+            request_method = " "
             print ("error: %s\n" % e)
             pass
         
         if request_method == 'GET':
-            print("Get method")
-        else:
-            self.request.sendall(bytearray("405 Method Not Allowed",'utf-8'))
+            file_path = self.data.splitlines()[0].decode().split(" ")[1]
+            if file_path == "/":
+                file_path = "/index.html"
 
+            valid_path, html_file = self.valid_path(file_path)
+
+            if valid_path:
+                file_type = file_path.split(".")[1]
+                self.request.sendall(bytearray("HTTP/1.1 200 OK\r\n",'utf-8'))
+                self.request.sendall(bytearray("Content-Type: text/%s\r\n"% file_type,'utf-8'))
+                self.request.sendall(bytearray("Content-Length: %s\r\n"% len(html_file),'utf-8'))
+                self.request.sendall(bytearray("Connection: keep-alive\r\n",'utf-8'))
+                self.request.sendall(bytearray(html_file,'utf-8'))
+
+            elif not valid_path:
+                self.request.sendall(bytearray("HTTP/1.1 404 Not Found!\r\n",'utf-8'))
+                self.request.sendall(bytearray("Content-Type: text/html\r\n",'utf-8'))
+                self.request.sendall(bytearray("Content-Length: %s\r\n"% len(html_file),'utf-8'))
+                self.request.sendall(bytearray("Connection: closed\r\n",'utf-8'))
+                self.request.sendall(bytearray(html_file,'utf-8'))
+
+        else:
+            html_file = open("www/405_error.html").read()
+            self.request.sendall(bytearray("HTTP/1.1 405 Method Not Allowed\r\n",'utf-8'))
+            self.request.sendall(bytearray("Content-Type: text/html\r\n",'utf-8'))
+            self.request.sendall(bytearray("Content-Length: %s\r\n"% len(html_file),'utf-8'))
+            self.request.sendall(bytearray("Connection: closed\r\n",'utf-8'))                
+            self.request.sendall(bytearray(html_file,'utf-8'))
+
+    def valid_path(self, file_path):
+        try:
+            html_file = open("www"+file_path).read()
+            return True, html_file
+
+        except FileNotFoundError:
+            html_file = open("www/404_error.html").read()
+            return False, html_file
 
 
 
